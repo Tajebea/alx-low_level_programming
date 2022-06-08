fee1abd33b1695c2c5cef5af2017dfcc6ef5e035
Tajebe <33959562+Tajebea@users.noreply.github.com>
2022-06-08 05:35:21 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-
-int main()
-{
-  int i;
-  putchar (' ');
-  for (i=1; i<=10; i++)
-    {
-      putchar ('_putchar');
-    }
-  putchar ('\n');
-
-  return 0;
-}
+ 
+ 
+  int main()
+ {
+      printf( "_putchar\n" );
+    return 0;
+  } 
