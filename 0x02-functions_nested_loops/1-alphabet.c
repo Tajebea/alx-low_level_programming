c1a6469eb141904d118d25bf1cfe24c836a2ec4f
Tajebea <tsegataju2017@gmail.com>
2022-06-08 07:06:28 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    print_alphabet();
-    return (0);
+for (char ch='a', ch<='z', ch++)
+{
+printf("%c",ch);
+}
+return (0);
 }
